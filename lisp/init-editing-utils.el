035ee2aec4ac7b526c20f7010af12c1a87972e4f
semgilo <9737935@qq.com>
2019-09-07 09:57:38 +0800

    disable auto create files

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -31,7 +31,11 @@
  set-mark-command-repeat-pop t
  tooltip-delay 1.5
  truncate-lines nil
- truncate-partial-width-windows nil)
+ truncate-partial-width-windows nil
+ make-backup-files nil
+ auto-save-default nil
+ create-lockfiles nil)
+
 
 (add-hook 'after-init-hook 'global-auto-revert-mode)
 (setq global-auto-revert-non-file-buffers t
