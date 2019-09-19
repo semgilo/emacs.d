6bd634768cdb129e2490960709cda86212acbb07
semgilo <9737935@qq.com>
2019-09-19 10:59:47 +0800

    modify python config

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -9,10 +9,21 @@
 
 (require-package 'pip-requirements)
 
+(defun setup-my-python-env ()
+  (setq indent-tabs-mode nil)
+  (setq tab-always-indent nil)
+  (setq python-indent-offset 4)
+  (setq python-indent 4)
+  (setq python-indent-guess-indent-offset-verbose nil)
+  (setq-default indent-tabs-mode nil)
+  (setq tab-width 4)
+  )
+
 (when (maybe-require-package 'anaconda-mode)
   (after-load 'python
     (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+    (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+    (add-hook 'python-mode-hook 'setup-my-python-env))
   (after-load 'anaconda-mode
     (define-key anaconda-mode-map (kbd "M-?") nil))
   (when (maybe-require-package 'company-anaconda)
